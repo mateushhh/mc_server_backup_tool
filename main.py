398bc13838d8c0263db4d962cc7d5f6538980007
Mateusz Grzonka <mateuszgrzonka2003@gmail.com>
2026-02-01 23:18:46 +0100

    Replaced file compression library, added progress info

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import shutil
 import datetime
+import zipfile
+import config
 
 DATETIME_STR = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+BACKUP_PATH = config.BACKUP_DIRECTORY + "/" + DATETIME_STR + "_backup.zip"
 
-SERVER_DIRECTORY = "F:/minecraft_server"
-BACKUP_DIRECTORY = "F:/minecraft_server_backup"
-BACKUP_PATH = BACKUP_DIRECTORY + "/" + DATETIME_STR + "_backup"
+progress_bar = 0
+progress_bar_max = len(config.DIRECTORIES_TO_BACKUP) + len(config.FILES_TO_BACKUP)
 
-print("Creating backup")
-shutil.make_archive(BACKUP_PATH, "zip", SERVER_DIRECTORY)
+def print_progress(filename):
+    global progress_bar, progress_bar_max
+    progress_bar += 1
+    print("(" + str(round(progress_bar / progress_bar_max * 100,1)) + "%)\t'" + filename + "' added to the backup")
+
 try:
-    shutil.make_archive(BACKUP_PATH, "zip", SERVER_DIRECTORY)
+    print("Creating backup file")
+    backup = zipfile.ZipFile(BACKUP_PATH, mode="w", compression=zipfile.ZIP_STORED)
+
+    for file in config.FILES_TO_BACKUP:
+        backup.write(config.SERVER_DIRECTORY+"/"+file)
+        print_progress(file)
+
+    # TODO Finish adding directories to the zipfile
+    for directory in config.DIRECTORIES_TO_BACKUP:
+        backup.mkdir(directory)
+        # Get inside the directory and recursively create and open each subdirectory and add each file
+        print_progress(directory)
+
     print("Backup complete \"" + BACKUP_PATH + "\" created")
 except Exception as e:
     print("Something went wrong: " + str(e))
--- readme.md
+++ readme.md
@@ -1,10 +1,11 @@
 # Minecraft Server Backup Tool (Fabric)
 
 ### How to use:
-- Set up `SERVER_DIRECTORY` and `BACKUP_DIRECTORY` in `config.cfg`
+- Set up `SERVER_DIRECTORY` and `BACKUP_DIRECTORY` in `config.py`
 - Run main.py with python
 
 ### TODO:
 - Make better config with presets for backups (choosing how many files to backup)
 - Add a progress bar
 - Switch to `zipfile` from `shutil` library
+- 
